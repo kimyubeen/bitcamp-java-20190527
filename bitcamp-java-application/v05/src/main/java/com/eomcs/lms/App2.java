b67fd546f66bb51c5b76a777b9707ed39f163ffe
kimyubeen <ybkim9606@naver.com>
2019-07-05 12:32:34 +0900

    bitcamp-java-application 05 버전

--- bitcamp-java-application/v05/src/main/java/com/eomcs/lms/App2.java
+++ bitcamp-java-application/v05/src/main/java/com/eomcs/lms/App2.java
@@ -1,42 +1,63 @@
 package com.eomcs.lms;
 
 import java.util.Scanner;
+import java.sql.Date;
 
 public class App2 {
+  static Scanner keyScan;
+  
   public static void main(String[] args) {
     java.io.InputStream keyboard = System.in;
-    Scanner keyScan = new Scanner(keyboard);
+    keyScan = new Scanner(keyboard);
     
-    System.out.print("번호? ");
-    String no = keyScan.nextLine();
+    int no = getIntValue("번호? ");
+    String name = getStringValue("이름? ");
+    String email = getStringValue("이메일? ");
+    String pw = getStringValue("암호? ");
+    String picture = getStringValue("사진? ");
+    String phoneNumber = getStringValue("전화? ");
+    java.sql.Date registeredDate = getDateValue("가입일? ");
     
-    System.out.print("이름? ");
-    String name = keyScan.nextLine();
-    
-    System.out.print("이메일? ");
-    String email = keyScan.nextLine();
-    
-    System.out.print("암호? ");
-    String pw = keyScan.nextLine();
-    
-    System.out.print("사진? ");
-    String picture = keyScan.nextLine();
-    
-    System.out.print("전화? ");
-    String phoneNumber = keyScan.nextLine();
-    
-    System.out.print("가입일? ");
-    String joinDate = keyScan.nextLine();
+    keyScan.close();
     
     System.out.println();
     
-    System.out.println("번호: " + no);
-    System.out.println("이름: " + name);
-    System.out.println("이메일: " + email);
-    System.out.println("암호: " + pw);
-    System.out.println("사진: " + picture);
-    System.out.println("전화: " + phoneNumber);
-    System.out.println("가입일: " + joinDate);
+    System.out.printf("번호: %d\n", no);
+    System.out.printf("이름: %s\n", name);
+    System.out.printf("이메일: %s\n", email);
+    System.out.printf("암호: %s\n", pw);
+    System.out.printf("사진: %s\n", picture);
+    System.out.printf("전화: %s\n", phoneNumber);
+    System.out.printf("가입일: %s\n", registeredDate);
+    
+  }
+  
+  public static int getIntValue(String message) {
+    int value = 0;
+    while (true) {
+      try {
+        System.out.print(message);
+        return Integer.parseInt(keyScan.nextLine());
+      } catch (NumberFormatException e) {
+        System.out.println("숫자를 입력하세요.");
+      }
+    }
+  }
+  
+  public static String getStringValue(String message) {
+    System.out.printf(message);
+    return keyScan.nextLine();
+  }
+  
+  public static java.sql.Date getDateValue(String message){
+    while (true) {
+      try {
+        System.out.print(message);
+        return java.sql.Date.valueOf(keyScan.nextLine());
+      } catch (IllegalArgumentException e) {
+        System.out.println("2019-07-04 형태로 입력하세요.");
+      }
+    }
   }
 
 }
--- bitcamp-java-application/v05/src/main/java/com/eomcs/lms/App3.java
+++ bitcamp-java-application/v05/src/main/java/com/eomcs/lms/App3.java
@@ -1,30 +1,57 @@
 package com.eomcs.lms;
 
+import java.sql.Date;
 import java.util.Scanner;
 
 public class App3 {
+  static Scanner keyScan;
+  
   public static void main(String[] args) {
     java.io.InputStream keyboard= System.in;
-    Scanner keyScan = new Scanner(keyboard);
+    keyScan = new Scanner(keyboard);
     
-    System.out.print("번호? ");
-    String no = keyScan.nextLine();
+    int no = getIntValue("번호? ");
+    String content = getStringValue("내용? ");
+    java.sql.Date createdDate = getDateValue("작성일? ");
+    int hits = getIntValue("조회수? ");
     
-    System.out.print("내용? ");
-    String content = keyScan.nextLine();
-    
-    System.out.print("작성일? ");
-    String dateOfPreparation = keyScan.nextLine();
-    
-    System.out.print("조회수? ");
-    String hits = keyScan.nextLine();
+    keyScan.close();
     
     System.out.println();
     
-    System.out.println("번호: " + no);
-    System.out.println("내용: " + content);
-    System.out.println("작성일: " + dateOfPreparation);
-    System.out.println("조회수: " + hits);
+    System.out.printf("번호: %d\n", no);
+    System.out.printf("내용: %s\n", content);
+    System.out.printf("작성일: %s\n", createdDate);
+    System.out.printf("조회수: %d\n", hits);
+    
+  }
+
+  private static int getIntValue(String message) {
+    int value = 0;
+    while (true) {
+      try {
+        System.out.print(message);
+        return Integer.parseInt(keyScan.nextLine());
+      } catch (NumberFormatException e) {
+        System.out.println("숫자를 입력하세요.");
+      }
+    }
+  }
+  
+  private static String getStringValue(String message) {
+    System.out.printf(message);
+    return keyScan.nextLine();
+  }
+  
+  private static Date getDateValue(String message) {
+    while (true) {
+      try {
+        System.out.print(message);
+        return java.sql.Date.valueOf(keyScan.nextLine());
+      } catch (IllegalArgumentException e) {
+        System.out.println("2019-07-04 형태로 입력하세요.");
+      }
+    }
   }
 
 }
