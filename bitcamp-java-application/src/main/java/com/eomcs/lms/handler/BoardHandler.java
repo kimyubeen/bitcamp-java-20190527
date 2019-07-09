43121736550c09dc3713eadfbc2443f9ddc0ea61
kimyubeen <ybkim9606@naver.com>
2019-07-09 17:24:27 +0900

    bitcamp-java-application v13 백업 완료

--- bitcamp-java-application/src/main/java/com/eomcs/lms/handler/BoardHandler.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/handler/BoardHandler.java
@@ -6,12 +6,14 @@ import com.eomcs.lms.domain.Board;
 import com.eomcs.lms.util.Input;
 
 public class BoardHandler {
+  // 인스턴스마다 개별적으로 다뤄야하는 것은 인스턴스 field로 만든다.
+  private Board[] boards = new Board[100];
+  private int boardsSize = 0;
   
-  private static Board[] boards = new Board[100];
-  private static int boardsSize = 0;
+  // 인스턴스와 상관없이 공유하는 데이터이면 static
   public static Scanner keyScan;
 
-  public static void addBoard() {
+  public void addBoard() {
     Board board = new Board();
     
     board.no = Input.getIntValue("번호? ");
@@ -24,10 +26,11 @@ public class BoardHandler {
     System.out.println("저장하였습니다.");
   }
   
-  public static void listBoard() {
+  public void listBoard() {
     for (int i = 0; i < boardsSize; i++) {
       Board board = boards[i]; //위의 두 줄을 한번에
-      System.out.printf("%d, %-20s , %-10s, %d\n", board.no, board.contents, board.createDate, board.viewCount);
+      System.out.printf("%d, %-20s , %-10s, %d\n", board.no, board.contents, board.createDate, 
+          board.viewCount);
     }
   }
 
