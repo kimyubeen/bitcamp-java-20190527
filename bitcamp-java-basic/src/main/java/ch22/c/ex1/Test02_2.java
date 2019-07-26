7bfb9fe9a4901f078c34597abae0531badd49d07
kimyubeen <ybkim9606@naver.com>
2019-07-26 18:00:05 +0900

    20190726

--- bitcamp-java-basic/src/main/java/ch22/c/ex1/Test02_2.java
+++ bitcamp-java-basic/src/main/java/ch22/c/ex1/Test02_2.java
@@ -1,27 +1,29 @@
-// 버퍼 사용 - 버퍼를 적용하여 데이터 읽기를 대신 처리해주는 도우미 클래스 사용
+// 버퍼 사용(for 텍스트 파일) - 사용 후
 // 
 package ch22.c.ex1;
 
-public class Test01_3 {
+import java.io.FileReader;
+
+public class Test02_2 {
   public static void main(String[] args) {
     
     try {
-      BufferedInputStream in = new BufferedInputStream("temp/jls12.pdf");
+      FileReader in = new FileReader("temp/jls12.txt");
       
       System.out.println("데이터 읽는 중...");
       
       long start = System.currentTimeMillis();
       
-      int b;
+      char[] buf = new char[8192];
+      int len = 0;
       int count = 0;
-      while ((b = in.read()) != -1) { // FileInputStream에 있는 read()가 아니라 BufferedInputStream에서 재정의 한 read()
+      while ((len = in.read(buf)) != -1) {
         count++;
       }
       
       long end = System.currentTimeMillis();
       System.out.println(end - start);
       System.out.println(count);
-      System.out.println(in.count);
       
       in.close();
       
--- bitcamp-java-basic/src/main/java/ch22/c/ex1/Test02_3.java
+++ bitcamp-java-basic/src/main/java/ch22/c/ex1/Test02_3.java
@@ -1,12 +1,12 @@
-// 버퍼 사용 - 버퍼를 적용하여 데이터 읽기를 대신 처리해주는 도우미 클래스 사용
+// 버퍼 사용(for 텍스트 파일) - 버퍼를 적용하여 데이터 읽기를 대신 처리해주는 도우미 클래스 사용
 // 
 package ch22.c.ex1;
 
-public class Test01_3 {
+public class Test02_3 {
   public static void main(String[] args) {
     
     try {
-      BufferedInputStream in = new BufferedInputStream("temp/jls12.pdf");
+      BufferedReader in = new BufferedReader("temp/jls12.txt");
       
       System.out.println("데이터 읽는 중...");
       
@@ -14,7 +14,7 @@ public class Test01_3 {
       
       int b;
       int count = 0;
-      while ((b = in.read()) != -1) { // FileInputStream에 있는 read()가 아니라 BufferedInputStream에서 재정의 한 read()
+      while ((b = in.read()) != -1) {
         count++;
       }
       
