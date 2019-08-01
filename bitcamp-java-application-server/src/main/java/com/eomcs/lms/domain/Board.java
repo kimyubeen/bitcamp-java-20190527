a54142c33c3864e2c1ce681d4e4cf104436755b3
kimyubeen <ybkim9606@naver.com>
2019-08-01 17:54:35 +0900

    bitcamp-java-applicaion-server v32

---
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/domain/Board.java
@@ -0,0 +1,46 @@
+package com.eomcs.lms.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Board implements Serializable {
+  private static final long serialVersionUID = 1L;
+  
+  private int no;
+  private String contents;
+  private Date createDate;
+  private int viewCount;
+  
+  @Override
+  public String toString() {
+    return "Board [no=" + no + ", contents=" + contents + ", createDate=" + createDate
+        + ", viewCount=" + viewCount + "]";
+  }
+  
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getContents() {
+    return contents;
+  }
+  public void setContents(String contents) {
+    this.contents = contents;
+  }
+  public Date getCreateDate() {
+    return createDate;
+  }
+  public void setCreateDate(Date createDate) {
+    this.createDate = createDate;
+  }
+  public int getViewCount() {
+    return viewCount;
+  }
+  public void setViewCount(int viewCount) {
+    this.viewCount = viewCount;
+  }
+
+  
+}
---
+++ bitcamp-java-application-server/v32_5/src/main/java/com/eomcs/lms/domain/Member.java
@@ -0,0 +1,68 @@
+package com.eomcs.lms.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Member implements Serializable {
+  private static final long serialVersionUID = 1L;
+  
+  private int no;
+  private String name;
+  private String email;
+  private String password;
+  private String photo;
+  private String tel;
+  private Date registerdDate;
+  
+  @Override
+  public String toString() {
+    return "Member [no=" + no + ", name=" + name + ", email=" + email + ", password=" + password
+        + ", photo=" + photo + ", tel=" + tel + ", registerdDate=" + registerdDate + "]";
+  }
+  
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public String getPhoto() {
+    return photo;
+  }
+  public void setPhoto(String photo) {
+    this.photo = photo;
+  }
+  public String getTel() {
+    return tel;
+  }
+  public void setTel(String tel) {
+    this.tel = tel;
+  }
+  public Date getRegisterdDate() {
+    return registerdDate;
+  }
+  public void setRegisterdDate(Date registerdDate) {
+    this.registerdDate = registerdDate;
+  }
+  
+  
+}
+
