147eb0bd45e0ccd8f4e95b956f8537708663fdda
kimyubeen <ybkim9606@naver.com>
2019-08-27 18:19:54 +0900

    bitcamp-java-application

---
+++ bitcamp-java-application4-server/v43_2/src/main/java/com/eomcs/lms/dao/mariadb/LessonDaoImpl.java
@@ -0,0 +1,71 @@
+package com.eomcs.lms.dao.mariadb;
+
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import com.eomcs.lms.dao.LessonDao;
+import com.eomcs.lms.domain.Lesson;
+
+public class LessonDaoImpl implements LessonDao {
+  SqlSessionFactory sqlSessionFactory;
+
+  public LessonDaoImpl(SqlSessionFactory sqlSessionFactory) {
+    this.sqlSessionFactory = sqlSessionFactory;
+  }
+
+  @Override
+  public int insert(Lesson lesson) throws Exception {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      int count = sqlSession.insert("LessonDao.insert", lesson);
+      sqlSession.commit();
+      return count;
+
+    } catch (Exception e) {
+      sqlSession.rollback();
+      throw e;
+
+    } finally {
+      sqlSession.close();
+    }
+
+  }
+
+  @Override
+  public List<Lesson> findAll() throws Exception {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      return sqlSession.selectList("LessonDao.findAll");
+    }
+  }
+
+  @Override
+  public Lesson findBy(int no) throws Exception {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      Lesson lesson = sqlSession.selectOne("LessonDao.findBy", no);
+      return lesson;
+
+    } catch (Exception e) {
+      throw e;
+
+    } finally {
+      sqlSession.close();
+    }
+  }
+
+  @Override
+  public int update(Lesson lesson) throws Exception {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+      return sqlSession.update("LessonDao.update", lesson);
+    }
+
+  }
+
+  @Override
+  public int delete(int no) throws Exception {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+      return sqlSession.delete("LessonDao.delete", no);
+    }
+
+  }
+}
