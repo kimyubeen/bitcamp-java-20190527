1d2f321689d6aea76565149eb03d56b41e184ed5
kimyubeen <ybkim9606@naver.com>
2019-08-29 16:36:48 +0900

    bitcamp-java-application

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/BoardDetailCommand.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/BoardDetailCommand.java
@@ -2,37 +2,41 @@ package com.eomcs.lms.handler;
 
 import java.io.BufferedReader;
 import java.io.PrintStream;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import com.eomcs.lms.dao.BoardDao;
 import com.eomcs.lms.domain.Board;
 import com.eomcs.util.Input;
 
 public class BoardDetailCommand implements Command {
-  
-  private BoardDao boardDao;
-  
-  public BoardDetailCommand(BoardDao boardDao) {
-    this.boardDao = boardDao;
+
+  private SqlSessionFactory sqlSessionFactory;
+
+  public BoardDetailCommand(SqlSessionFactory sqlSessionFactory) {
+    this.sqlSessionFactory = sqlSessionFactory;
   }
-  
+
   @Override
   public void execute(BufferedReader in, PrintStream out) {
 
-    try {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()){
+      BoardDao boardDao = sqlSession.getMapper(BoardDao.class);
+
       // 클라이언트에게 번호를 요구하여 받는다.
       int no = Input.getIntValue(in, out, "번호? ");
-      
+
       // 사용자가 입력한 번호를 가지고 목록에서 그 번호에 해당하는 Board 객체를 찾는다.
       Board board = boardDao.findBy(no);
-      
+
       if (board == null) {
         out.println("해당 번호의 데이터가 없습니다!");
         return;
       }
       boardDao.increaseViewCount(no);
-      
+
       out.printf("내용: %s\n", board.getContents());
       out.printf("작성일: %s\n", board.getCreatedDate());
-      
+
     } catch (Exception e) {
       out.println("데이터 조회에 실패했습니다!");
       System.out.println(e.getMessage());
