c9bb83883f3747576cc40e1c2b7955a3050662bf
kimyubeen <ybkim9606@naver.com>
2019-09-04 17:51:25 +0900

    bitcamp-java-application

--- bitcamp-java-basic/src/main/java/ch29/j/Test03.java
+++ bitcamp-java-basic/src/main/java/ch29/j/Test03.java
@@ -3,20 +3,17 @@ package ch29.j;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ch29.SpringUtils;
 
 public class Test03 {
   public static void main(String[] args) {
     
     ApplicationContext iocContainer = 
         new AnnotationConfigApplicationContext(AppConfig3.class);
-    
+
     System.out.println("---------------------------------------");
-    
-    String[] names = iocContainer.getBeanDefinitionNames();
-    for (String name : names) {
-      System.out.printf("%s ==> %s\n", 
-          name, iocContainer.getBean(name).getClass().getName());
-    }
+    SpringUtils.printObjects(iocContainer);
+    System.out.println("---------------------------------------");
   }
 }
 
--- bitcamp-java-basic/src/main/java/ch29/j/Test05.java
+++ bitcamp-java-basic/src/main/java/ch29/j/Test05.java
@@ -3,13 +3,13 @@ package ch29.j;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ch29.SpringUtils;
 
 public class Test05 {
   public static void main(String[] args) {
     
     ApplicationContext iocContainer = 
         new AnnotationConfigApplicationContext(AppConfig5.class);
-    
     System.out.println("---------------------------------------");
     
     String[] names = iocContainer.getBeanDefinitionNames();
