0d2f46b3c6d78690e4f7e7c9fde5856a77b1e67c
kimyubeen <ybkim9606@naver.com>
2019-09-05 11:29:27 +0900

    bitcamp-java-application

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/PhotoBoardCommand.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/PhotoBoardCommand.java
@@ -4,16 +4,20 @@ import java.io.BufferedReader;
 import java.io.PrintStream;
 import java.util.List;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import com.eomcs.lms.dao.PhotoBoardDao;
 import com.eomcs.lms.dao.PhotoFileDao;
 import com.eomcs.lms.domain.PhotoBoard;
 import com.eomcs.lms.domain.PhotoFile;
 import com.eomcs.util.Input;
-import com.eomcs.util.PlatformTransactionManager;
 import com.eomcs.util.RequestMapping;
 
 @Component
 public class PhotoBoardCommand {
+
   private PlatformTransactionManager txManager;
   private PhotoBoardDao photoBoardDao;
   private PhotoFileDao photoFileDao;
@@ -30,19 +34,25 @@ public class PhotoBoardCommand {
 
   @RequestMapping("/photoboard/add") // 클라이언트 요청이 들어 왔을 때 이 메서드를 호출하라고 표시한다.
   public void add(BufferedReader in, PrintStream out) {
+    // 트랜잭션 동작을 정의한다.
+    DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+    def.setName("tx1");
+    def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+    // 정의된 트랜잭션 동작에 따라 작업을 수행할 트랜잭션 객체를 준비한다.
+    TransactionStatus status = txManager.getTransaction(def);
+
     try {
-      txManager.beginTransaction();
-      
       PhotoBoard photoBoard = new PhotoBoard();
       photoBoard.setTitle(Input.getStringValue(in, out, "제목? "));
       photoBoard.setLessonNo(Input.getIntValue(in, out, "수업? "));
-      
+
       photoBoardDao.insert(photoBoard);
-      
+
       out.println("최소 한 개의 사진 파일을 등록해야 합니다.");
       out.println("파일명 입력 없이 그냥 엔터를 치면 파일 추가를 마칩니다.");
       out.flush();
-      
+
       int count = 0;
       while (true) {
         String filepath = Input.getStringValue(in, out, "사진 파일? ");
@@ -60,25 +70,30 @@ public class PhotoBoardCommand {
         photoFileDao.insert(photoFile);
         count++;
       }
-      
-      txManager.commit();
+
+      txManager.commit(status);
       out.println("저장하였습니다.");
-      
+
     } catch (Exception e) {
-      try {txManager.rollback();} catch (Exception e2) {}
-      
+      txManager.rollback(status);
       out.println("데이터 저장에 실패했습니다!");
       System.out.println(e.getMessage());
       e.printStackTrace();
     }
-     
+
   }
-  
+
   @RequestMapping("/photoboard/delete") // 클라이언트 요청이 들어 왔을 때 이 메서드를 호출하라고 표시한다.
   public void delete(BufferedReader in, PrintStream out) {
+    // 트랜잭션 동작을 정의한다.
+    DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+    def.setName("tx1");
+    def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+    // 정의된 트랜잭션 동작에 따라 작업을 수행할 트랜잭션 객체를 준비한다.
+    TransactionStatus status = txManager.getTransaction(def);
+
     try {
-      txManager.beginTransaction();
-      
       int no = Input.getIntValue(in, out, "번호? ");
 
       if (photoBoardDao.findBy(no) == null) {
@@ -92,50 +107,50 @@ public class PhotoBoardCommand {
       // 게시물을 삭제한다.
       photoBoardDao.delete(no);
 
-      txManager.commit();
+      txManager.commit(status);
       out.println("사진을 삭제했습니다.");
 
     } catch (Exception e) {
-      try {txManager.rollback();} catch (Exception e2) {}
+      txManager.rollback(status);
       out.println("데이터를 삭제에 실패했습니다.");
       System.out.println(e.getMessage());
     }
   }
-  
+
   @RequestMapping("/photoboard/detail") // 클라이언트 요청이 들어 왔을 때 이 메서드를 호출하라고 표시한다.
   public void detail(BufferedReader in, PrintStream out) {
 
     try {
       // 클라이언트에게 번호를 요구하여 받는다.
       int no = Input.getIntValue(in, out, "번호? ");
-      
+
       // 사용자가 입력한 번호를 가지고 목록에서 그 번호에 해당하는 PhotoBoard 객체를 찾는다.
       PhotoBoard photoBoard = photoBoardDao.findWithFilesBy(no);
-      
+
       if (photoBoard == null) {
         out.println("해당 번호의 데이터가 없습니다!");
         return;
       }
-      
+
       photoBoardDao.increaseViewCount(no);
-      
+
       out.printf("제목: %s\n", photoBoard.getTitle());
       out.printf("작성일: %s\n", photoBoard.getCreatedDate());
       out.printf("조회수: %s\n", photoBoard.getViewCount());
       out.printf("수업: %s\n", photoBoard.getLessonNo());
       out.println("사진파일:");
-      
+
       List<PhotoFile> files = photoBoard.getFiles();
       for (PhotoFile file : files) {
         out.printf("> %s\n", file.getFilePath());
       }
-      
+
     } catch (Exception e) {
       out.println("데이터 조회에 실패했습니다!");
       System.out.println(e.getMessage());
     }
   }
-  
+
   @RequestMapping("/photoboard/list") // 클라이언트 요청이 들어 왔을 때 이 메서드를 호출하라고 표시한다.
   public void list(BufferedReader in, PrintStream out) {
     try {
@@ -145,18 +160,23 @@ public class PhotoBoardCommand {
             photoBoard.getNo(), photoBoard.getTitle(), 
             photoBoard.getCreatedDate(), photoBoard.getViewCount(), photoBoard.getLessonNo());
       }
-      
+
     } catch (Exception e) {
       out.println("데이터 목록 조회에 실패했습니다!");
       System.out.println(e.getMessage());
     }
   }
-  
+
   @RequestMapping("/photoboard/update") // 클라이언트 요청이 들어 왔을 때 이 메서드를 호출하라고 표시한다.
   public void update(BufferedReader in, PrintStream out) {
+    // 트랜잭션 동작을 정의한다.
+    DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+    def.setName("tx1");
+    def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+    // 정의된 트랜잭션 동작에 따라 작업을 수행할 트랜잭션 객체를 준비한다.
+    TransactionStatus status = txManager.getTransaction(def);
     try {
-      txManager.beginTransaction();
-      
       int no = Input.getIntValue(in, out, "번호? ");
       PhotoBoard photoBoard = photoBoardDao.findBy(no);
 
@@ -217,11 +237,11 @@ public class PhotoBoardCommand {
         count++;
       }
 
-      txManager.commit();
+      txManager.commit(status);
       out.println("사진을 변경하였습니다.");
 
     } catch (Exception e) {
-      try {txManager.rollback();} catch (Exception e2) {}
+      txManager.rollback(status);
       out.println("데이터 변경에 실패했습니다.");
       System.out.println(e.getMessage());
     }
