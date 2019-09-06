eb8415bd5e82e75a8ff861a1b400f1b1efd921a7
kimyubeen <ybkim9606@naver.com>
2019-09-06 12:12:53 +0900

    bitcamp-java-application

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
@@ -25,47 +25,47 @@ public class App {
 
   // Log4j의 로그 출력 도구를 준비한다.
   private static final Logger logger = LogManager.getLogger(App.class);
-
+  
   ApplicationContext appCtx;
   RequestMappingHandlerMapping handlerMapping;
   int state;
-
+  
   public App() throws Exception {
     appCtx = new AnnotationConfigApplicationContext(AppConfig.class);
-
+    
     // Spring IoC 컨테이너에 들어 있는(Spring IoC 컨테이너가 생성한) 객체 알아내기
     String[] beanNames = appCtx.getBeanDefinitionNames();
-    logger.debug("[Spring IoC 컨테이너 객체들]----------------");
+    logger.debug("[Spring IoC 컨테이너 객체들]------------");
     for (String beanName : beanNames) {
-      logger.debug(String.format("%s(%s)", 
+      logger.debug(String.format("%s(%s)",
           appCtx.getBean(beanName).getClass().getSimpleName(),
           beanName));
     }
-    logger.debug("--------------------------------------");
-
+    logger.debug("------------------------------------");
+    
     handlerMapping = createRequestMappingHandlerMapping();
-
   }
 
   private RequestMappingHandlerMapping createRequestMappingHandlerMapping() {
-    RequestMappingHandlerMapping mapping =
+    
+    RequestMappingHandlerMapping mapping = 
         new RequestMappingHandlerMapping();
-
+    
     // 객체풀에서 @Component 애노테이션이 붙은 객체 목록을 꺼낸다.
     Map<String,Object> components = appCtx.getBeansWithAnnotation(Component.class);
-
-    logger.trace("[요청 핸들러] =======================================");
-
+    
+    logger.trace("[요청 핸들러] ===========================");
+    
     // 객체 안에 선언된 메서드 중에서 @RequestMapping이 붙은 메서드를 찾아낸다.
     Collection<Object> objList = components.values();
     objList.forEach(obj -> {
-
+      
       Method[] methods = null;
-
+      
       if (AopUtils.isAopProxy(obj)) { // 원본이 아니라 프록시 객체라면
         try {
           // 프록시 객체의 클래스가 아니라 원본 객체의 클래스 정보를 가져온다.
-          Class<?> originClass =
+          Class<?> originClass = 
               (Class<?>) obj.getClass().getMethod("getTargetClass").invoke(obj);
           methods = originClass.getMethods();
         } catch (Exception e) {
@@ -75,19 +75,20 @@ public class App {
         // 원본 객체의 클래스로부터 메서드 목록을 가져온다.
         methods = obj.getClass().getMethods();
       }
-
+      
       for (Method m : methods) {
         RequestMapping anno = m.getAnnotation(RequestMapping.class);
         if (anno == null)
           continue;
-
-        // @RequestMapping이 붙은 메서드를 찾으면 mapping객체에 보관한다.
+        // @RequestMapping 이 붙은 메서드를 찾으면 mapping 객체에 보관한다.
         mapping.addRequestHandler(anno.value()[0], obj, m);
-        logger.trace(String.format("%s ==> %s.%s()",
-            anno.value()[0], obj.getClass().getSimpleName(), m.getName()));
+        logger.trace(String.format("%s ==> %s.%s()", anno.value()[0], 
+            obj.getClass().getSimpleName(),
+            m.getName()));
       }
+      
     });
-
+    
     return mapping;
   }
 
@@ -104,21 +105,21 @@ public class App {
         .setServerInfo("Bitcamp/1.1")
         .setSocketConfig(socketConfig)
         .setSslContext(null)
-        .setExceptionLogger(ex ->{
-          if (ex instanceof SocketTimeoutException) {
-            System.err.println("Connection timed out");
-          } else if (ex instanceof ConnectionClosedException) {
-            System.err.println(ex.getMessage());
-          } else {
-            ex.printStackTrace();
-          }
-        })
+        .setExceptionLogger(ex -> {
+            if (ex instanceof SocketTimeoutException) {
+              System.err.println("Connection timed out");
+            } else if (ex instanceof ConnectionClosedException) {
+              System.err.println(ex.getMessage());
+            } else {
+              ex.printStackTrace();
+            }
+          })
         .registerHandler("*", new CommandHttpRequestHandler(handlerMapping))
         .create();
 
     server.start();
     logger.info("서버 실행 중...");
-
+    
     server.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
 
     Runtime.getRuntime().addShutdownHook(new Thread() {
@@ -136,6 +137,7 @@ public class App {
 
     } catch (Exception e) {
       logger.info("시스템 실행 중 오류 발생!");
+      
       StringWriter out2 = new StringWriter();
       e.printStackTrace(new PrintWriter(out2));
       logger.debug(out2.toString());
