3c66a29e937e78e4d9ef584e01627a8ed0a38ad6
kimyubeen <ybkim9606@naver.com>
2019-09-06 12:09:34 +0900

    bitcamp-java-application

---
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/HelloCommand.java
@@ -0,0 +1,16 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
+public class HelloCommand {
+
+  @RequestMapping("/hello") // 클라이언트 요청이 들어 왔을 때 이 메서드를 호출하라고 표시한다.
+  public void execute(BufferedReader in, PrintWriter out) {
+    out.println("<html><body><h1>안녕하세요!</h1><p>오호라...</p></body></html>");
+  }
+
+}
