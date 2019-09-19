8ea1e9753f3f5e422f1df4ca8da4b6c6b79c4241
kimyubeen <ybkim9606@naver.com>
2019-09-19 11:50:51 +0900

    bitcamp-java-application

---
+++ bitcamp-java-application4-server/v56_9/src/main/java/com/eomcs/lms/dao/PhotoFileDao.java
@@ -0,0 +1,13 @@
+package com.eomcs.lms.dao;
+
+import java.util.List;
+import com.eomcs.lms.domain.PhotoFile;
+
+// 게시판 관리 DAO 사용 규칙을 정의한다.
+public interface PhotoFileDao {
+  
+  int insert(PhotoFile photoFile) throws Exception;
+  List<PhotoFile> findAll(int boardNo) throws Exception;
+  int deleteAll(int boardNo) throws Exception;
+  
+}
